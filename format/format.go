a909f947341f86d5cdcba11622da79141b53e195
Dan <91132936+DanOBrain@users.noreply.github.com>
2026-03-18 07:30:20 +0300

    Update format.go

--- format/format.go
+++ format/format.go
@@ -1,1 +1,36 @@
+package format
 
+import (
+	"fmt"
+	"json-client/models"
+)
+
+// Formatter - вывод
+type Formatter struct{}
+
+// NewFormatter - конструктор вывода
+func NewFormatter() *Formatter {
+	return &Formatter{}
+}
+
+// Вывод поста
+func (f *Formatter) PrintPost(p models.Post) {
+	fmt.Printf("\nПост %d\n", p.ID)
+	fmt.Printf("	Заголовок: %s\n", p.Title)
+	fmt.Printf("	Содержание: %s\n", p.Body)
+	fmt.Println("")
+}
+
+// Вывод комментария
+func (f *Formatter) PrintComment(c models.Comment) {
+	fmt.Printf("\nКомментарий %d\n", c.ID)
+	fmt.Printf("	Название: %s\n", c.Name)
+	fmt.Printf("	Почта: %s\n", c.Email)
+	fmt.Printf("	Содержание: %s\n", c.Body)
+	fmt.Println("")
+}
+
+// Вывод ошибки
+func (f *Formatter) PrintError(err error) {
+	fmt.Printf("Ошибка: %v\n", err)
+}
